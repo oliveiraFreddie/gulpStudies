857a60041223e948598a47a0242a1df1c530b133
oliveiraFreddie <freddie.oliveira95@gmail.com>
2023-11-03 09:22:14 +0000

    gulp/ts

--- typeScript/src/app.ts
+++ typeScript/src/app.ts
@@ -1,10 +1,10 @@
 import { Carro, Vendavel } from './produto'
 
 function exibir(v: Vendavel) {
-    console.log(`${v.nome} custa ${v.preco}!`)
+  console.log(`${v.nome} custa ${v.preco}!`)
 }
 
-const c = new Carro
+const c = new Carro()
 c.nome = 'Civic'
-c.preco = 89499.00
+c.preco = 89499.0
 exibir(c)
